27374d4702cd3c7c126dd7dd35f5f9c1112a3a5a
Kira Kawai <asymptote.k.k@gmail.com>
2021-01-24 17:23:07 +0900

    入力を受け取る

--- main.go
+++ main.go
@@ -10,17 +10,24 @@ import (
 )
 
 type City struct {
-	ID          int    `json:"id,omitempty" db:"ID"`
-	Name        string `json:"name,omitempty" db:"Name"`
-	CountryCode string `json:"countryCode,omitempty" db:"CountryCode"`
-	District    string `json:"district,omitempty" db:"District"`
-	Population  int    `json:"population,omitempty" db:"Population"`
+	ID          int     `json:"id,omitempty" db:"ID"`
+	Name        string  `json:"name,omitempty" db:"Name"`
+	CountryCode string  `json:"countryCode,omitempty" db:"CountryCode"`
+	District    string  `json:"district,omitempty" db:"District"`
+	Population  int     `json:"population,omitempty" db:"Population"`
+}
+
+type Country struct {
+	Code       string `json:"code,omitempty"  db:"Code"`
+	Name       string `json:"name,omitempty"  db:"Name"`
+	Continent  string `json:"continent,omitempty"  db:"Continent"`
+	Population int    `json:"population,omitempty"  db:"Population"`
 }
 
 func main() {
 	db, err := sqlx.Connect("mysql", fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
-		os.Getenv("DB_USENAME"),
+		os.Getenv("DB_USERNAME"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOSTNAME"),
 		os.Getenv("DB_PORT"),
@@ -30,8 +37,13 @@ func main() {
 	}
 
 	fmt.Println("Connected!")
-	city := City{}
-	_ = db.Get(&city, "SELECT * FROM city WHERE Name = 'Tokyo'")
 
-	fmt.Printf("Tokyoの人口は%d人です\n", city.Population)
+	city := City{}
+	_ = db.Get(&city, "SELECT * FROM city WHERE Name = '" + os.Args[1] + "' LIMIT 1")
+
+	country := Country{}
+	_ = db.Get(&country, "SELECT Code, Population FROM country WHERE Code = '" + city.CountryCode + "' LIMIT 1")
+
+	fmt.Printf("%vの人口割合は%vです\n", os.Args[1], float64(city.Population) / float64(country.Population))
 }
+
